904069deb0f25d7af4a901fe86f177a0a180e65b
Pratyush Pratik Sinha <pratyushpratiksinha@gmail.com>
2026-06-30 01:16:57 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        var seen = Set(nums)
+        var output = 0
+
+        for i in 0..<nums.count {
+            var currentNum = nums[i]
+            var currentStreak = 1
+
+            while seen.contains(currentNum + 1) {
+                currentStreak += 1
+                currentNum += 1
+            }
+            output = max(output, currentStreak)
+        }
+        return output
+    }
+}
